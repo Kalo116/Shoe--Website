6a786b9914337e2087958bd35d0c0e096160a2af
Kaloyan Yordanov <kaloyan0102@gmail.com>
2023-03-28 00:36:38 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import { Header } from './components/heaeder-component/header.component';
+
+import { HomePage } from './components/home-page-component/homePage.component';
+import { SignInPage } from './components/sign-in-page-component/sign-in.component';
+import { SignUpPage } from './components/sign-up-page-component/sign-up.component';
+import { Contact } from './components/contact-page-component/contact.component';
+import { Testimonial } from './components/testimonial-page-component/testimonial.component';
+import { Shop } from './components/shop-page-component/shop.component';
+import { NotFound } from './components/404-page-component/404.component';
+
+import { Footer } from './components/footer-component/footer.components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <>
+            <Header />
+            <main className="main">
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/sign-in' element={<SignInPage />} />
+                    <Route path='/sign-up' element={<SignUpPage />} />
+                    <Route path='/contacts' element={<Contact />} />
+                    <Route path='/testimonial' element={<Testimonial />} />
+                    <Route path='/shop' element={<Shop />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </main>
+            <Footer />
+
+        </>
+    )
+};
 
 export default App;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,81 @@
+import { initializeApp } from "firebase/app";
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from 'firebase/auth';
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDwNpNcbrIqKvu9GCBj7s_EJumxceYs6oA",
+    authDomain: "shoe-shop-website.firebaseapp.com",
+    projectId: "shoe-shop-website",
+    storageBucket: "shoe-shop-website.appspot.com",
+    messagingSenderId: "988561986146",
+    appId: "1:988561986146:web:09c589c5f64c59cc85773b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+    prompt: 'select_account'
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocument = async (userAuth, additionalInformation = {}) => {
+
+    if (!userAuth) return;
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalInformation
+            });
+        } catch (error) {
+            console.log('Error creating the user', error.message);
+        };
+    };
+
+    return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangeListener = (callback) => onAuthStateChanged(auth, callback);
